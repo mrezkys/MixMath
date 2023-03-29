5bc15e4b3269fdf509b9794c4b8bc79a48485abe
Vincent Gunawan <gnwnvincent@gmail.com>
2023-03-29 11:20:46 +0700

    Initial Commit

---
+++ MathOUITests/MathOUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  MathOUITestsLaunchTests.swift
+//  MathOUITests
+//
+//  Created by Vincent Gunawan on 29/03/23.
+//
+
+import XCTest
+
+final class MathOUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
