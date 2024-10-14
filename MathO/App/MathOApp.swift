13961c1bc0d4ba5197ae10295a6b6b6cc7060f57
Muhammad Rezky <mrezkysulihin@gmail.com>
2024-10-14 19:42:54 +0700

    style(main): reorganize folder for resources and app

---
+++ MathO/App/MathOApp.swift
@@ -0,0 +1,17 @@
+//
+//  MathOApp.swift
+//  MathO
+//
+//  Created by Vincent Gunawan on 29/03/23.
+//
+
+import SwiftUI
+
+@main
+struct MathOApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
